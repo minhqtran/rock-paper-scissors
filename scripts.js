04f9484b7cc91100ac609cf702838edf987e28d6
Kundaslayer <apple@Apples-MacBook-Pro.local>
2024-02-23 14:31:30 +0700

    add ui to the project

--- scripts.js
+++ scripts.js
@@ -1,40 +1,58 @@
-let answer = ['Rock', 'Paper', 'Scissors'];
-let score = 0;
+let answer = ['rock', 'paper', 'scissors'];
 
-function getComputerChoice() {
+let rockBtn = document.querySelector("#rock");
+let scissorsBtn = document.querySelector("#scissors")
+let paperBtn = document.querySelector("paper")
+
+rockBtn.addEventListener('click', () => playRound("rock"))
+scissorsBtn.addEventListener('click', () => playRound("scissors"))
+paperBtn.addEventListener('click', () => playRound("paper"))
+
+//get the computer choices randomly
+function computerPlay() {
   let random = Math.floor(Math.random() * answer.length);
   return answer[random];
 }
-
-function playRound(playerChoice,compChoice){
-  let x = String(playerChoice).toUpperCase();
-  let y = String(compChoice).toUpperCase();
-  if((x == "ROCK" && y == "SCISSORS")
-    || (x == "PAPER" && y == "ROCK")
-    || (x == "SCISSORS" && y == "PAPER")){
-      score++;
-      return "You win, " + playerChoice + " beats " + compChoice ;
-    }
-  else if(x.toUpperCase()  === y.toUpperCase()){
-      return "Draw";
-  }
-  else{
-    return "You lose, " + compChoice + " beats " + playerChoice;
+//function to compare. 
+let determineWinner = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection){
+    return 'tie'
+  } else if(
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    return 'player'
+  } else {
+    return 'computer'
   }
 }
 
-function game(){
-  for(let i = 1; i <= 5; i++){
-    let playerSelection = window.prompt("Enter your choice");
-    let computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-    
-  }  
-  return "You earn " + score + " points";
+function playRound(playerSelection){
+  const computerSelection = computerPlay()
+  const roundResult = determineWinner(playerSelection, computerSelection)
+
+  if(roundResult === 'player'){
+    playerScore++;
+  }else if(roundResult === 'computer'){
+    computerScore++;
+  }
 }
 
-let x = game();
+const resultDiv = document.querySelector("#result")
 
-console.log(x);
+let displayResult = (result) => resultDiv.textContent = result
+
+let displayScore = (playerScore, computerScore) =>  {
+  resultDiv.textContent += "\nPlayer: ${playerScore} - Computer: ${computerScore}";
+}
+
+let announceWinner = (playerScore, computerScore) => {
+  if(playerScore === 5 ){
+    resultDiv.textContent += '\n You win :)'
+  }else if (computerScore === 5){
+    resultDiv.textContent += '\n You lose :('
+  }
+}
 
 
